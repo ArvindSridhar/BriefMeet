d4544390f450dae124f71efcf24370a5f2e6216c
Brandon Soeung <br.soeung@gmail.com>
2017-10-07 13:34:51 -0700

    Commit adds basic connection to database and route to add user

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import views
+from pymongo import MongoClient
+
+
+def get_db():
+    from pymongo import MongoClient
+    DB_NAME = 'briefmeet-demo'
+    DB_HOST = 'ds036577.mlab.com'
+    DB_PORT = 36577
+    DB_USER = 'web'
+    DB_PASS = 'B0WLES-G0D$'
+
+    connection = MongoClient(DB_HOST, DB_PORT)
+    db = connection[DB_NAME]
+    db.authenticate(DB_USER, DB_PASS)
+    return db
+
+def add_person(db):
+    db.Users.insert({"name" : "check"})
+    
+def get_person(db):
+    return db.Users.find_one()
